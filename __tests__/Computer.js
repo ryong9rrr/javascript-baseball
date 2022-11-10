472edd20edc4db1958f9bf4d4312417ee3768772
yong <tkddbs1644@gmail.com>
2022-11-10 23:04:42 +0900

    chore: test 메세지를 상세히 작성

--- __tests__/Computer.js
+++ __tests__/Computer.js
@@ -8,7 +8,7 @@ const repeat = (callbackFn, trial = 100) => {
 };
 
 describe("Computer 단위 테스트", () => {
-  test("generateRandomNumber 메서드 100회 테스트", () => {
+  test("generateRandomNumber가 서로 다른 3자리 숫자를 반환하는지 100회 테스트", () => {
     repeat(() => {
       const randomNumber = Computer.generateRandomNumber();
       expect(Utils.removeDuplicatedString(randomNumber).length).toEqual(3);
--- __tests__/Validator.js
+++ __tests__/Validator.js
@@ -1,7 +1,7 @@
 const Validator = require("../src/domain/Validator");
 
 describe("Validator 단위 테스트", () => {
-  test("isValidNumber 메서드 테스트", () => {
+  test("isValidNumber 메서드로 올바른 입력 값인지 검사", () => {
     const TEST_CASE = [
       {
         input: "",
