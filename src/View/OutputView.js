bed8002ae7c6bddd4730c6677d861ab1bf0045f8
yong <tkddbs1644@gmail.com>
2022-12-02 00:30:29 +0900

    feat(OutputView): 결과를 출력하는 기능 구현

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -5,6 +5,22 @@ class OutputView {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
 
+  static printResult(ballCount, strikeCount) {
+    if (ballCount === 0 && strikeCount === 0) {
+      Console.print("낫싱");
+      return;
+    }
+    if (ballCount === 0) {
+      Console.print(`${strikeCount}스트라이크`);
+      return;
+    }
+    if (strikeCount === 0) {
+      Console.print(`${ballCount}볼`);
+      return;
+    }
+    Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+  }
+
   static printEnd() {
     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
