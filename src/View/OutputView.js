0f65e413fa888064a7ea0917838ae3ae5507bec9
yong <tkddbs1644@gmail.com>
2022-12-02 00:11:46 +0900

    feat(OutputView): 게임 종료 메세지를 출력하는 기능 구현

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -4,6 +4,10 @@ class OutputView {
   static printStart() {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
+
+  static printEnd() {
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
 }
 
 module.exports = OutputView;
