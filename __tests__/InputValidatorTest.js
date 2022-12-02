eb364d6fb663cd99d6338c2a7d7390b4d05f19e9
yong <tkddbs1644@gmail.com>
2022-12-02 20:20:53 +0900

    refactor(InputValidator): 메서드를 정적메서드로 변경

--- __tests__/InputValidatorTest.js
+++ __tests__/InputValidatorTest.js
@@ -1,13 +1,11 @@
 const InputValidator = require("../src/InputValidator");
 
-const validator = new InputValidator();
-
 describe("입력값 유효성 통합테스트", () => {
   test("입력값이 길이가 3이 아니면 예외 발생", () => {
     const TEST_CASE = ["ab", "1234", "11"];
     TEST_CASE.forEach((input) => {
       expect(() => {
-        validator.validate(input);
+        InputValidator.validate(input);
       }).toThrow("[ERROR]");
     });
   });
@@ -16,7 +14,7 @@ describe("입력값 유효성 통합테스트", () => {
     const TEST_CASE = ["abc", "c12", "12a", " 123", "123 ", "   "];
     TEST_CASE.forEach((input) => {
       expect(() => {
-        validator.validate(input);
+        InputValidator.validate(input);
       }).toThrow("[ERROR]");
     });
   });
@@ -25,7 +23,7 @@ describe("입력값 유효성 통합테스트", () => {
     const TEST_CASE = ["102"];
     TEST_CASE.forEach((input) => {
       expect(() => {
-        validator.validate(input);
+        InputValidator.validate(input);
       }).toThrow("[ERROR]");
     });
   });
@@ -34,7 +32,7 @@ describe("입력값 유효성 통합테스트", () => {
     const TEST_CASE = ["121"];
     TEST_CASE.forEach((input) => {
       expect(() => {
-        validator.validate(input);
+        InputValidator.validate(input);
       }).toThrow("[ERROR]");
     });
   });
--- src/InputValidator.js
+++ src/InputValidator.js
@@ -3,20 +3,20 @@ const ERROR_MESSAGE =
   "[ERROR] 0을 제외한 각 자리 수가 서로 다른 3자리의 숫자를 입력해주세요.";
 
 class InputValidator {
-  validate(input) {
+  static validate(input) {
     const numbers = input.split("").map((number) => Number(number));
-    this.#checkLength(numbers);
-    this.#checkEachNumber(numbers);
-    this.#checkDuplicate(numbers);
+    InputValidator.checkLength(numbers);
+    InputValidator.checkEachNumber(numbers);
+    InputValidator.checkDuplicate(numbers);
   }
 
-  #checkLength(numbers) {
+  static checkLength(numbers) {
     if (numbers.length !== LENGTH) {
       throw new Error(ERROR_MESSAGE);
     }
   }
 
-  #checkEachNumber(numbers) {
+  static checkEachNumber(numbers) {
     numbers.forEach((number) => {
       if (Number.isNaN(Number(number)) || number === 0) {
         throw new Error(ERROR_MESSAGE);
@@ -24,7 +24,7 @@ class InputValidator {
     });
   }
 
-  #checkDuplicate(numbers) {
+  static checkDuplicate(numbers) {
     const isDuplicated = [...new Set(numbers)].length !== LENGTH;
     if (isDuplicated) {
       throw new Error(ERROR_MESSAGE);
