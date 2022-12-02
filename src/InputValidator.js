b3c6fcc1ab28d7c5f74a700d961987e50e7abc10
yong <tkddbs1644@gmail.com>
2022-12-02 20:26:24 +0900

    refactor(InputValidator): 메서드 이름, 에러 네이밍 등 변경

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -1,9 +1,18 @@
 const LENGTH = 3;
-const ERROR_MESSAGE =
-  "[ERROR] 0을 제외한 각 자리 수가 서로 다른 3자리의 숫자를 입력해주세요.";
+const ERRORS = Object.freeze({
+  NUMBER:
+    "[ERROR] 0을 제외한 각 자리 수가 서로 다른 3자리의 숫자를 입력해주세요.",
+  REPLAY: "[ERROR] 1또는 2를 눌러주세요.",
+});
 
 class InputValidator {
-  static validate(input) {
+  static validateReplayAnswer(answer) {
+    if (answer !== "1" || answer !== "2") {
+      throw new Error(ERRORS.REPLAY);
+    }
+  }
+
+  static validateInputNumber(input) {
     const numbers = input.split("").map((number) => Number(number));
     InputValidator.checkLength(numbers);
     InputValidator.checkEachNumber(numbers);
@@ -12,14 +21,14 @@ class InputValidator {
 
   static checkLength(numbers) {
     if (numbers.length !== LENGTH) {
-      throw new Error(ERROR_MESSAGE);
+      throw new Error(ERRORS.NUMBER);
     }
   }
 
   static checkEachNumber(numbers) {
     numbers.forEach((number) => {
       if (Number.isNaN(Number(number)) || number === 0) {
-        throw new Error(ERROR_MESSAGE);
+        throw new Error(ERRORS.NUMBER);
       }
     });
   }
@@ -27,7 +36,7 @@ class InputValidator {
   static checkDuplicate(numbers) {
     const isDuplicated = [...new Set(numbers)].length !== LENGTH;
     if (isDuplicated) {
-      throw new Error(ERROR_MESSAGE);
+      throw new Error(ERRORS.NUMBER);
     }
   }
 }
