28104e3f4f00544b89de42d8801ead2a8c6a249f
yong <tkddbs1644@gmail.com>
2022-12-02 21:06:13 +0900

    feat(App): 숫자 야구 게임 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+const { Console } = require("@woowacourse/mission-utils");
+const BaseBallGame = require("./domain/BaseBallGame");
+const Computer = require("./domain/Computer");
+const InputView = require("./View/InputView");
+const OutputView = require("./View/OutputView");
+
 class App {
-  play() {}
+  play() {
+    OutputView.printStart();
+    this.showStart();
+  }
+
+  showStart() {
+    this.baseBallGame = new BaseBallGame(Computer.generateRandomNumbers);
+    this.showReadNumber();
+  }
+
+  showReadNumber() {
+    InputView.readNumber((numbers) => this.showReadNumberCallback(numbers));
+  }
+
+  showReadNumberCallback(numbers) {
+    const ballCount = this.baseBallGame.getBallCount(numbers);
+    const strikeCount = this.baseBallGame.getStrikeCount(numbers);
+    OutputView.printResult(ballCount, strikeCount);
+    if (strikeCount !== 3) {
+      this.showReadNumber();
+      return;
+    }
+    OutputView.printEnd();
+    this.showReadReplay();
+  }
+
+  showReadReplay() {
+    InputView.readReplay((answer) => this.showReadReplayCallback(answer));
+  }
+
+  showReadReplayCallback(answer) {
+    if (answer === "1") {
+      this.showStart();
+      return;
+    }
+    Console.close();
+  }
 }
 
 module.exports = App;
